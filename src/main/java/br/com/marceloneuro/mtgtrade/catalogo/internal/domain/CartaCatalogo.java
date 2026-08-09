f1085af179d28c0407b0da49eb17f433b708abd5
marcelo-neuro <marcelofurla0813@gmail.com>
2026-08-09 13:41:06 -0300

    Feat: entidade CartaCatalogo mapeada

--- src/main/java/br/com/marceloneuro/mtgtrade/catalogo/internal/domain/CartaCatalogo.java
+++ src/main/java/br/com/marceloneuro/mtgtrade/catalogo/internal/domain/CartaCatalogo.java
@@ -1,4 +1,37 @@
 package br.com.marceloneuro.mtgtrade.catalogo.internal.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "catalogo_cartas")
+@Data
+@EqualsAndHashCode(of = "id")
+@AllArgsConstructor
+@NoArgsConstructor
 public class CartaCatalogo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "oracle_id", nullable = false)
+    private String oracleId;
+
+    @Column(name = "print_id", nullable = false, unique = true)
+    private String printId;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String edicao;
+
+    @Column(nullable = false)
+    private String imagemUrl;
 }
