a7ae1dc882501e5c88c14898615c9eb5223aceef
marcelo-neuro <marcelofurla0813@gmail.com>
2026-08-11 20:46:58 -0300

    chore: configura RestClient para requisições do Scryfall

---
+++ src/main/java/br/com/marceloneuro/mtgtrade/shared/RestClientConfig.java
@@ -0,0 +1,20 @@
+package br.com.marceloneuro.mtgtrade.shared;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public RestClient ScryfallRestClient(RestClient.Builder builder) {
+        return builder
+                .baseUrl("https://api.scryfall.com")
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.USER_AGENT, "Mtg-Trade-projeto/0.1.0")
+                .build();
+    }
+}
