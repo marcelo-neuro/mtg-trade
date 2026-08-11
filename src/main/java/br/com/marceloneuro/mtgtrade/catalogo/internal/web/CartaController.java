abef1e7c2d338da99962b20c37946f9f6a0b99db
marcelo-neuro <marcelofurla0813@gmail.com>
2026-08-11 12:20:50 -0300

    refactor: altera rota do endpoint de carta para o plural

--- src/main/java/br/com/marceloneuro/mtgtrade/catalogo/internal/web/CartaController.java
+++ src/main/java/br/com/marceloneuro/mtgtrade/catalogo/internal/web/CartaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/carta")
+@RequestMapping("/api/v1/cartas")
 @RequiredArgsConstructor
 public class CartaController {
 
