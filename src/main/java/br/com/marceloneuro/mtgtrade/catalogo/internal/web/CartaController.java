352f4ec5da88117bd447e0840681103911af7435
marcelo-neuro <marcelofurla0813@gmail.com>
2026-08-17 20:36:08 -0300

    feat: adiciona endpoint ao CartaController para realizar a sincronização do banco de dados com o Scryfall

--- src/main/java/br/com/marceloneuro/mtgtrade/catalogo/internal/web/CartaController.java
+++ src/main/java/br/com/marceloneuro/mtgtrade/catalogo/internal/web/CartaController.java
@@ -2,14 +2,12 @@ package br.com.marceloneuro.mtgtrade.catalogo.internal.web;
 
 import br.com.marceloneuro.mtgtrade.catalogo.api.dto.CartaCatalogoDTO;
 import br.com.marceloneuro.mtgtrade.catalogo.internal.application.service.CatalogoService;
+import br.com.marceloneuro.mtgtrade.catalogo.internal.infrastructure.scryfall.SincronizacaoCatalogoJob;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/cartas")
@@ -17,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CartaController {
 
     private final CatalogoService catalogoService;
+    private final SincronizacaoCatalogoJob sincronizacaoCatalogoJob;
 
     @GetMapping
     public ResponseEntity<Page<CartaCatalogoDTO>> buscarPorNome(
@@ -24,5 +23,12 @@ public class CartaController {
             Pageable pageable) {
 
         return ResponseEntity.ok(catalogoService.buscarCartasPorNome(nome, pageable));
+    }
+
+    @PostMapping("/sincronizar")
+    public ResponseEntity<String> sincronizarScryfall() {
+        sincronizacaoCatalogoJob.sincronizaCatalogo();
+
+        return ResponseEntity.ok("Sincronização completa.");
     }
 }
