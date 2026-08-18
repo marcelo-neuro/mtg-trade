8121ebbacdecc37c3500e654070055ca80c5fdff
marcelo-neuro <marcelofurla0813@gmail.com>
2026-08-18 13:26:09 -0300

    perf: configura pageable padrão para requisições de busca por nome em CartaController

--- src/main/java/br/com/marceloneuro/mtgtrade/catalogo/internal/web/CartaController.java
+++ src/main/java/br/com/marceloneuro/mtgtrade/catalogo/internal/web/CartaController.java
@@ -6,6 +6,7 @@ import br.com.marceloneuro.mtgtrade.catalogo.internal.infrastructure.scryfall.Si
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +21,7 @@ public class CartaController {
     @GetMapping
     public ResponseEntity<Page<CartaCatalogoDTO>> buscarPorNome(
             @RequestParam(required = false, defaultValue = "") String nome,
-            Pageable pageable) {
+            @PageableDefault(size = 20, sort = "nome") Pageable pageable) {
 
         return ResponseEntity.ok(catalogoService.buscarCartasPorNome(nome, pageable));
     }
