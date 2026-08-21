43802152f4ed3753bde3540549c30fd47de0fede
marcelo-neuro <marcelofurla0813@gmail.com>
2026-08-21 18:29:40 -0300

    chore: move RestClientConfig para shared.config

--- src/main/java/br/com/marceloneuro/mtgtrade/shared/config/RestClientConfig.java
+++ src/main/java/br/com/marceloneuro/mtgtrade/shared/config/RestClientConfig.java
@@ -1,4 +1,4 @@
-package br.com.marceloneuro.mtgtrade.shared;
+package br.com.marceloneuro.mtgtrade.shared.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
