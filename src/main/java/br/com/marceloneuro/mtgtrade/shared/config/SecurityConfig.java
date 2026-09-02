bd1011cfdea0dcb53812a78c8d7fec7440992b12
marcelo-neuro <marcelofurla0813@gmail.com>
2026-09-02 13:01:20 -0300

    chore: adiciona lançamento de exceção na configuração do SecurityFilterChain

--- src/main/java/br/com/marceloneuro/mtgtrade/shared/config/SecurityConfig.java
+++ src/main/java/br/com/marceloneuro/mtgtrade/shared/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
