32b2b249dce2568322715cd8759a590a2671a621
marcelo-neuro <marcelofurla0813@gmail.com>
2026-09-04 14:02:36 -0300

    feat: cria implementação da interface UserDetailsService

---
+++ src/main/java/br/com/marceloneuro/mtgtrade/iam/internal/application/service/ImplUserDetailService.java
@@ -0,0 +1,22 @@
+package br.com.marceloneuro.mtgtrade.iam.internal.application.service;
+
+import br.com.marceloneuro.mtgtrade.iam.internal.infrastructure.UsuarioRepository;
+import br.com.marceloneuro.mtgtrade.iam.internal.infrastructure.security.ImplUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImplUserDetailService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new ImplUserDetails(usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("E-mail não existente.")));
+    }
+}
