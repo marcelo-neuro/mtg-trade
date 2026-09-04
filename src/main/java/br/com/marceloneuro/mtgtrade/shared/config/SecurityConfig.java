88bbdaa76120315ce2f7b71fd796bc513ebdd1a1
marcelo-neuro <marcelofurla0813@gmail.com>
2026-09-04 14:12:42 -0300

    chore: configura bean para injeção do AuthenticationManager

--- src/main/java/br/com/marceloneuro/mtgtrade/shared/config/SecurityConfig.java
+++ src/main/java/br/com/marceloneuro/mtgtrade/shared/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package br.com.marceloneuro.mtgtrade.shared.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,5 +27,10 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> req.anyRequest().permitAll())
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
+        return configuration.getAuthenticationManager();
     }
 }
