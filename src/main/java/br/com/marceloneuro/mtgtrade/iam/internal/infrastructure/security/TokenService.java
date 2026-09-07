34dadad4891926ad76b837eadf91055de0047e15
marcelo-neuro <marcelofurla0813@gmail.com>
2026-09-07 15:00:00 -0300

    chore: remove dependência inutilizada

--- src/main/java/br/com/marceloneuro/mtgtrade/iam/internal/infrastructure/security/TokenService.java
+++ src/main/java/br/com/marceloneuro/mtgtrade/iam/internal/infrastructure/security/TokenService.java
@@ -2,7 +2,6 @@ package br.com.marceloneuro.mtgtrade.iam.internal.infrastructure.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
