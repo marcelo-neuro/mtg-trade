d04019c05764e2130b05b4740efe8fbc13a230a2
marcelo-neuro <marcelofurla0813@gmail.com>
2026-09-07 18:11:59 -0300

    feat: adiciona tratamento da exception MethodArgumentNotValidException

--- src/main/java/br/com/marceloneuro/mtgtrade/shared/web/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/marceloneuro/mtgtrade/shared/web/exception/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package br.com.marceloneuro.mtgtrade.shared.web.exception;
 
 import br.com.marceloneuro.mtgtrade.shared.web.exception.dto.ErroDTO;
+import br.com.marceloneuro.mtgtrade.shared.web.exception.dto.MensagemCampo;
+import br.com.marceloneuro.mtgtrade.shared.web.exception.dto.ValidationErroDTO;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,6 +24,20 @@ public class GlobalExceptionHandler {
                 status.value(), Instant.now());
 
         return ResponseEntity.status(status).body(erro);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErroDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_CONTENT;
+
+        ValidationErroDTO erros = new ValidationErroDTO(e.getMessage(), request.getRequestURI(),
+                status.value(), Instant.now());
+
+        e.getBindingResult()
+                .getFieldErrors()
+                .forEach(fe -> erros.adicionarCampo(new MensagemCampo(fe.getField(), fe.getDefaultMessage())));
+
+        return ResponseEntity.status(status.value()).body(erros);
     }
 
 }
