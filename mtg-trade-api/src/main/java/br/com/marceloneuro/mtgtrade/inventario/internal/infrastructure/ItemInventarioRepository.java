a0b62959232f29a493e15856f7d937ae09c434a8
marcelo-neuro <marcelofurla0813@gmail.com>
2026-09-20 15:58:18 -0300

    feat: adiciona query para encontrar item que já existe no inventário do usuário

--- mtg-trade-api/src/main/java/br/com/marceloneuro/mtgtrade/inventario/internal/infrastructure/ItemInventarioRepository.java
+++ mtg-trade-api/src/main/java/br/com/marceloneuro/mtgtrade/inventario/internal/infrastructure/ItemInventarioRepository.java
@@ -1,9 +1,16 @@
 package br.com.marceloneuro.mtgtrade.inventario.internal.infrastructure;
 
+import br.com.marceloneuro.mtgtrade.inventario.internal.domain.Estado;
+import br.com.marceloneuro.mtgtrade.inventario.internal.domain.Idioma;
 import br.com.marceloneuro.mtgtrade.inventario.internal.domain.ItemInventario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ItemInventarioRepository extends JpaRepository<ItemInventario, UUID> {
+
+    Optional<ItemInventario> findByUsuarioIdAndCartaCatalogoIdAndAcabamentoAndPromoAndEstadoAndIdioma(UUID usuarioId, UUID cartaCatalogoId,
+                                                                                      String acabamento, String promo,
+                                                                                      Estado estado, Idioma idioma);
 }
