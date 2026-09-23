812c32e8dfeee60c081e6e4b76c0eb6d43b43c25
marcelo-neuro <marcelofurla0813@gmail.com>
2026-09-22 22:34:32 -0300

    fix: corrige findByUsuarioId para retornar um Page

--- mtg-trade-api/src/main/java/br/com/marceloneuro/mtgtrade/inventario/internal/infrastructure/ItemInventarioRepository.java
+++ mtg-trade-api/src/main/java/br/com/marceloneuro/mtgtrade/inventario/internal/infrastructure/ItemInventarioRepository.java
@@ -3,9 +3,10 @@ package br.com.marceloneuro.mtgtrade.inventario.internal.infrastructure;
 import br.com.marceloneuro.mtgtrade.inventario.internal.domain.Estado;
 import br.com.marceloneuro.mtgtrade.inventario.internal.domain.Idioma;
 import br.com.marceloneuro.mtgtrade.inventario.internal.domain.ItemInventario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -17,5 +18,5 @@ public interface ItemInventarioRepository extends JpaRepository<ItemInventario, 
 
     Optional<ItemInventario> findByIdAndUsuarioId(UUID id, UUID usuarioId);
 
-    List<ItemInventario> findByUsuarioId(UUID usuarioId);
+    Page<ItemInventario> findByUsuarioId(UUID usuarioId, Pageable pageable);
 }
