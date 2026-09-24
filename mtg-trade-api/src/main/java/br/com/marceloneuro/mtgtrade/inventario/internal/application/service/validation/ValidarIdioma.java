bbc30c21085dcbceb87106cc78f3d722f9349906
marcelo-neuro <marcelofurla0813@gmail.com>
2026-09-24 16:13:34 -0300

    feat: implementa validação de idioma

---
+++ mtg-trade-api/src/main/java/br/com/marceloneuro/mtgtrade/inventario/internal/application/service/validation/ValidarIdioma.java
@@ -0,0 +1,19 @@
+package br.com.marceloneuro.mtgtrade.inventario.internal.application.service.validation;
+
+import br.com.marceloneuro.mtgtrade.catalogo.api.dto.CartaCatalogoDTO;
+import br.com.marceloneuro.mtgtrade.inventario.internal.application.service.dto.AtributosFisicosRequest;
+import br.com.marceloneuro.mtgtrade.inventario.internal.domain.Idioma;
+
+import java.util.Arrays;
+
+public class ValidarIdioma implements ValidadorItemInventario {
+    @Override
+    public void validar(AtributosFisicosRequest request, CartaCatalogoDTO cartaCatalogo) {
+        boolean idiomaValido = Arrays.stream(Idioma.values())
+                .anyMatch(idioma -> idioma.name().equalsIgnoreCase(request.idioma().trim()));
+
+        if (!idiomaValido) {
+            throw new IllegalArgumentException("Idioma não existente.");
+        }
+    }
+}
